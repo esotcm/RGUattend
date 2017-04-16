5df86feed051e33280dbb12d3d847896daa08ad5
anything <pauliusshiu@gmail.com>
2017-04-16 01:59:38 +0100

    init

--- database/DBconnect.php
+++ database/DBconnect.php
@@ -1,10 +1,10 @@
 <?php
 
 $serverName = "
-rguattend.database.windows.net";
-$DB_name = "paulius";
-$DB_password = "Admin20k";
-$DB = "RGUattend";
+eu-cdbr-azure-west-d.cloudapp.net";
+$DB_name = "b50e4e852f567b";
+$DB_password = "434b386b";
+$DB = "attendants";
 //attempting to connect to the database
 $conn = new mysqli($serverName, $DB_name, $DB_password, $DB);
 //Letting the user know if the connection didnt work
