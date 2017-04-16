5ce5e0c3df348c28bcdc76e1b5eca97196a0ce9a
anything <pauliusshiu@gmail.com>
2017-04-16 12:59:18 +0100

    init

--- sing.php
+++ sing.php
@@ -1,8 +1,9 @@
 <?php
 include("database/DBConnect.php");
-$studentID = $_POST["studentID"];
+$studentID = $_GET["studentID"];
 $sql = "INSERT INTO attending (studentID) VALUES ('$studentID')";
 if(mysqli_query($conn, $sql)){
 }else{
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+?>
